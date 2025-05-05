7278bed4589e3652835dea991c58e8be440a8880
Мария Федорова <fedorova.md@phystech.edu>
2025-05-05 18:33:23 +0300

    Основные файлы + структура FastAPI

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Сервис для оценки и поиска книг",
+    description="Основано на фреймворке FastAPI.",
+    version="0.0.1",
+    contact={
+        "name": "Федорова Мария",
+        "url": "https://github.com/FMariaD/FastAPI_Webapp",
+        "email": "fedorova.md@phystech.edu",
+    },
+)
+
+origins = ["https://localhost:8087", "http://localhost:8087", "localhost:8087", "*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Это проект для курса Программирование на Python (2 семестр)"}
+
+
+# app.include_router(router)
